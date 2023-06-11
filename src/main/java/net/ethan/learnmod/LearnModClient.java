676174835c73f1eaef3816c5bba971a57496a502
Ethan <apollo11rocketship@gmail.com>
2023-06-11 12:46:22 -0400

    Initial commit

---
+++ src/main/java/net/ethan/learnmod/LearnModClient.java
@@ -0,0 +1,10 @@
+package net.ethan.learnmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
