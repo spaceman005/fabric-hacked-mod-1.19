d6eb645d0981de52bfd5aa057b07551a639709fe
Ethan <apollo11rocketship@gmail.com>
2023-06-11 21:40:23 -0400

    fly hack!!!

---
+++ src/main/java/net/ethan/learnmod/FlyHack.java
@@ -0,0 +1,50 @@
+package net.ethan.learnmod;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+
+public class FlyHack {
+    private static boolean flyhackEnabled = true;
+    public static void setFlyhackEnabled(boolean enabled) {
+        flyhackEnabled = enabled;
+    }
+    static int tickCount = 0;
+    public static void tick(MinecraftClient client) {
+        if (flyhackEnabled) {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            if (player != null && player.world.isClient) {
+                Vec3d velocity = player.getVelocity();
+
+                double motionY = 0;
+                double motionX = velocity.x;
+                double motionZ = velocity.z;
+                if (client.options.jumpKey.isPressed()) {
+                    motionY += 0.5;
+                }
+                else if (client.options.sneakKey.isPressed()) {
+                    motionY -= 0.35;
+                }
+                if (client.options.forwardKey.isPressed() && !player.isOnGround()){
+                    double speed = 0.35;
+                    float yawRad = player.getYaw() * MathHelper.RADIANS_PER_DEGREE;
+
+                    motionX = MathHelper.sin(-yawRad) * speed;
+                    motionZ = MathHelper.cos(yawRad) * speed;
+                }
+                //sets velocity based on actions taken by the player
+                player.setVelocity(new Vec3d(motionX, motionY, motionZ));
+
+                if (tickCount >= 35) {
+                    if (!player.isOnGround()){
+                        Vec3d pos = player.getVelocity();
+                        player.setVelocity(pos.x, -0.37, pos.z);
+                    }
+                    tickCount = 0;
+                }
+                tickCount++;
+            }
+        }
+    }
+}
--- src/main/java/net/ethan/learnmod/LearnMod.java
+++ src/main/java/net/ethan/learnmod/LearnMod.java
@@ -2,15 +2,21 @@ package net.ethan.learnmod;
 
 import net.ethan.learnmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LearnMod implements ModInitializer {
 	public static final String MOD_ID = "learnmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
+		//fly hack
+		ClientTickEvents.START_CLIENT_TICK.register(client -> {
+			FlyHack.tick(client);
+		});
+		FlyHack.setFlyhackEnabled(true);
+		//legit
 		ModItems.registerModItems();
 	}
 }
